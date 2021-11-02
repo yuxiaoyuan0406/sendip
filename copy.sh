66e35eb47b66749728e39093922095755c3818d7
yuxiaoyuan0406 <yuxiaoyuan0406@qq.com>
2021-11-02 15:54:45 +0800

    copy files to where it should be

--- copy.sh
+++ copy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 sudo cp send_ip.py /usr/local/bin/
-sudo cp sendip.service /usr/lib/systemd/system/
+sudo cp sendip.service /etc/systemd/system/
+sudo mkdir /etc/sendip -p
+chmod a+x sendip_st*
+sudo cp sendip_st* /etc/sendip/
+sudo cp email.json /etc/sendip/
 sudo systemctl daemon-reload
 sudo systemctl restart sendip.service
 systemctl status sendip.service
