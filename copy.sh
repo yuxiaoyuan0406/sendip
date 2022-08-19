301c2791e03b4815a4f04b4a411badc76b4fbad1
yuxiaoyuan0406 <973971990@qq.com>
2022-08-19 09:15:09 +0800

    more steps

--- copy.sh
+++ copy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 sudo cp send_ip.py /usr/local/bin/
-sudo cp sendip.service /etc/systemd/system/
+sudo cp sendip.service /usr/lib/systemd/system/
 sudo mkdir /etc/sendip -p
 chmod a+x sendip_st*
 sudo cp sendip_st* /etc/sendip/
 sudo cp email.json /etc/sendip/
+sudo systemctl disable sendip.service
 sudo systemctl daemon-reload
+sudo systemctl enable sendip.service
 sudo systemctl restart sendip.service
 systemctl status sendip.service
